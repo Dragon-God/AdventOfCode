8446ff2422fcdbf270141308a4e7bd87cd8a16c5
Alafin Oluwatobi <tobi.alafin@gmail.com>
2019-12-02 18:25:43 +0100

    2019 Day 1

---
+++ 2019/Solutions/day_1.py
@@ -0,0 +1,13 @@
+import os
+
+f = lambda mass: mass // 3 - 2
+
+def part_one():
+  with open(r"../Inputs/day_1.txt") as file:
+    return sum(f(int(i)) for i in file.readlines())
+
+def part_two():
+  def partial_sum(mass):
+    return 0 if f(mass) <= 0 else f(mass) + partial_sum(f(mass))
+  with open(r"../Inputs/day_1.txt") as file:
+    return sum(partial_sum(int(mass)) for mass in file.readlines())
